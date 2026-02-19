b7b59be578ce04fa759b7096e26d2972ba401aab
Daniel Benavides <d.benavidess@uniandes.edu.co>
2026-02-19 11:53:39 -0500

    Added project instructions (PDF), and two separate csv readers for raw and processed data

---
+++ csv_reader_raw.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+def read_csv(file_path):
+    """
+    Reads a CSV file and returns a cleaned DataFrame.
+
+    Parameters:
+    file_path (str): The path to the CSV file.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing only string values for all data.
+    """
+
+    try:
+        df = pd.read_csv(file_path, dtype=str, low_memory=False)
+        df = df.apply(lambda col: col.str.strip('"'))
+
+        print(f"Datos cargados: {df.shape[0]} filas, {df.shape[1]} columnas")
+        return df
+    except Exception as e:
+        print(f"An error occurred while reading the CSV file: {e}")
+        return None
+
+def save_csv(df, output_path='resultados_saber11.csv'):
+    """
+    Saves a DataFrame to a CSV file.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to save.
+    output_path (str): The path to the output CSV file. Defaults to 'resultados_saber11.csv'.
+    """
+    
+    try:
+        df.to_csv(output_path, index=False)
+        print(f"Archivo guardado en: {output_path}")
+    except Exception as e:
+        print(f"An error occurred while saving the CSV file: {e}")
+
+csv_data = read_csv('resultados_saber11_raw.csv')
+
+if csv_data is not None:
+    save_csv(csv_data)
