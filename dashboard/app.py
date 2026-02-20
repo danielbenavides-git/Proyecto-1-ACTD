33f24a95ba10d3d4bb0a5d19ba0650f249d85f35
Juanita Cortes <juanitacova@gmail.com>
2026-02-20 08:32:40 -0500

    primer dash intento

--- dashboard/app.py
+++ dashboard/app.py
@@ -219,5 +219,4 @@ def update_graph(mun_value, chart_type):
     return fig
 
 if __name__ == "__main__":
-    # Dash moderno:
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=True, host="127.0.0.1", port=8051)
